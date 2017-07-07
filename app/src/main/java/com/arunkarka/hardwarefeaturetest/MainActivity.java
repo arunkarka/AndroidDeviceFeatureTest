c2285ce2b441737d90e35f4712ecaacab8d25974
arun.karka <arunkarka@gmail.com>
2017-07-07 14:33:30 -0500

    Added changes to test location.gps feature.

--- app/src/main/java/com/arunkarka/hardwarefeaturetest/MainActivity.java
+++ app/src/main/java/com/arunkarka/hardwarefeaturetest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.arunkarka.hardwarefeaturetest;
 
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> featureList = new ArrayList<>();
+        featureList.add(PackageManager.FEATURE_LOCATION_GPS);
+        PackageManager pm = getPackageManager();
+        TextView tv = (TextView) findViewById(R.id.tv);
+        for (String feature : featureList) {
+            if (!pm.hasSystemFeature(feature)) {
+                tv.setText(feature+" is NOT available \n");
+            } else {
+                tv.setText(feature+" is available \n");
+            }
+        }
     }
 }
